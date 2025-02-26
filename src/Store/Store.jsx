6ccd768623e134786c05413523aefc35682b7cf0
Bhraltin <bhriskts10@gmail.com>
2025-02-26 15:07:30 +0300

    verceltest

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,9 +1,9 @@
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
 import {thunk} from "redux-thunk"
 import {logger} from "redux-logger"
-import clientReducer from "./reducers/clientReducer"
-import productReducer from './reducers/productReducer';
-import shoppingCartReducer from './reducers/shoppingCartReducer';
+import clientReducer from "./reducers/clientReducer.jsx"
+import productReducer from './reducers/productReducer.jsx';
+import shoppingCartReducer from './reducers/shoppingCartReducer.jsx';
 
 const rootReducer = combineReducers({
     client: clientReducer,
