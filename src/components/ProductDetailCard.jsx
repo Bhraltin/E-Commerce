c7cd84038f3de87c7c223b46fdf5cd7becdf924b
Bhraltin <bhriskts10@gmail.com>
2025-02-27 11:40:57 +0300

    ProductData added

---
+++ src/components/ProductDetailCard.jsx
@@ -0,0 +1,24 @@
+export default function ProductDetailCard () {
+
+    const product = {
+        id: 1,
+        name: "Floating Phone",
+        price: 1139.33,
+        rating: 4,
+        reviewCount: 10,
+        description:
+          "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
+        availability: "In Stock",
+        colors: ["blue", "green", "orange", "navy"],
+        images: [
+          "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/desktop-product-cards-42-OzRo0nlzRCw8Tebp0kjm0HT5QVAEL2.png",
+          "/placeholder.svg?height=400&width=400",
+          "/placeholder.svg?height=400&width=400",
+        ],
+      }
+
+
+    return (
+       <div></div>
+    )
+}
