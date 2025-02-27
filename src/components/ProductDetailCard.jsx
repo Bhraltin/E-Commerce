c73fe4d02f03b253d09549f30b2137ab6d2676a4
Bhraltin <bhriskts10@gmail.com>
2025-02-27 11:42:49 +0300

    imported

--- src/components/ProductDetailCard.jsx
+++ src/components/ProductDetailCard.jsx
@@ -1,21 +1,27 @@
-export default function ProductDetailCard () {
+import { useState } from "react"
+import { Heart, ShoppingCart, Eye, ChevronLeft, ChevronRight } from "lucide-react"
 
-    const product = {
-        id: 1,
-        name: "Floating Phone",
-        price: 1139.33,
-        rating: 4,
-        reviewCount: 10,
-        description:
-          "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
-        availability: "In Stock",
-        colors: ["blue", "green", "orange", "navy"],
-        images: [
-          "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/desktop-product-cards-42-OzRo0nlzRCw8Tebp0kjm0HT5QVAEL2.png",
-          "/placeholder.svg?height=400&width=400",
-          "/placeholder.svg?height=400&width=400",
-        ],
-      }
+const product = {
+    id: 1,
+    name: "Floating Phone",
+    price: 1139.33,
+    rating: 4,
+    reviewCount: 10,
+    description:
+      "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
+    availability: "In Stock",
+    colors: ["blue", "green", "orange", "navy"],
+    images: [
+      "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/desktop-product-cards-42-OzRo0nlzRCw8Tebp0kjm0HT5QVAEL2.png",
+      "/placeholder.svg?height=400&width=400",
+      "/placeholder.svg?height=400&width=400",
+    ],
+  }
+
+
+export default function ProductDetailCard ({product}) {
+
+
 
 
     return (
