b50ed1cac201b0eaea2b75051d121e4cfca87991
Bhraltin <bhriskts10@gmail.com>
2025-02-27 11:52:33 +0300

    ProductDetailCard over

--- src/components/pages/ProductDetailPage.jsx
+++ src/components/pages/ProductDetailPage.jsx
@@ -1,8 +1,29 @@
 import Layout from "../layout/Layout";
 import {Link} from "react-router-dom"
 import { ChevronRight } from "lucide-react";
+import ProductDetailCard from "../ProductDetailCard";
+
 
 export default function ProductDetailPage () {
+
+
+    const product = {
+        id: 1,
+        name: "Floating Phone",
+        price: 1139.33,
+        rating: 4,
+        reviewCount: 10,
+        description:
+          "Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.",
+        availability: "In Stock",
+        colors: ["blue", "green", "orange", "navy"],
+        images: [
+          "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/desktop-product-cards-42-OzRo0nlzRCw8Tebp0kjm0HT5QVAEL2.png",
+          "/placeholder.svg?height=400&width=400",
+          "/placeholder.svg?height=400&width=400",
+        ],
+      }
+
     return (
         <Layout>
             <div className=" bg-gray-50">
@@ -24,6 +45,7 @@ export default function ProductDetailPage () {
         </div>
       </div>
       </div>
+      <ProductDetailCard product={product}/>
         </Layout>
     )
 }
